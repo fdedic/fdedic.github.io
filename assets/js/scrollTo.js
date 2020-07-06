5c006d9b77ce954e14ebddd11371b00037a68714
fdedic <fdedic.noreply.github.com>
2020-07-06 16:43:52 +0200

    S

--- assets/js/scrollTo.js
+++ assets/js/scrollTo.js
@@ -1,13 +1,4 @@
-jQuery(document).ready(function() {
-				var offset = 100;
-				var duration = 750;
-				jQuery(window).scroll(function() {
 
-				});
-
-				jQuery('.scroll-top').click(function(event) {
-					event.preventDefault();
-					jQuery('html, body').animate({scrollTop: 0}, duration);
-					return false;
-				})
-			});
+      function scrollTo(hash) {
+          location.hash = "#" + možnosti-spolupráce;
+      }
